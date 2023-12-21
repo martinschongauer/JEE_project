2e1844f8fcb293586af30146f9d70c84e59df21e
JeromeGermain <mail.jeromegermain@gmail.com>
2023-12-21 17:12:58 +0100

    utilisateursDTO ok, utilsiateursService et repo 90%

--- JEE_REST/src/main/java/m2i/projet/JEE_REST/dto/UtilisateurDTO.java
+++ JEE_REST/src/main/java/m2i/projet/JEE_REST/dto/UtilisateurDTO.java
@@ -1,5 +1,78 @@
 package m2i.projet.JEE_REST.dto;
 
-public class UtilisateurDTO {
+import java.util.Objects;
 
+import m2i.projet.JEE_REST.entity.Utilisateur;
+
+public class UtilisateurDTO {
+	private String nomUser;
+	private String prenomUser;
+	private String mailUser;
+	private Integer roleUser;
+	public String getNomUser() {
+		return nomUser;
+	}
+	public void setNomUser(String nomUser) {
+		this.nomUser = nomUser;
+	}
+	public String getPrenomUser() {
+		return prenomUser;
+	}
+	public void setPrenomUser(String prenomUser) {
+		this.prenomUser = prenomUser;
+	}
+	public String getMailUser() {
+		return mailUser;
+	}
+	public void setMailUser(String mailUser) {
+		this.mailUser = mailUser;
+	}
+	public Integer getRoleUser() {
+		return roleUser;
+	}
+	public void setRoleUser(Integer roleUser) {
+		this.roleUser = roleUser;
+	}
+	
+	/**Constructeur d'utilisateurDTO par paramètres**/
+	public UtilisateurDTO(String nomUser, String prenomUser, String mailUser, Integer roleUser) {
+		super();
+		this.nomUser = nomUser;
+		this.prenomUser = prenomUser;
+		this.mailUser = mailUser;
+		this.roleUser = roleUser;
+	}
+	/**Constructeur d'utilisateurDTO par defaut**/
+	public UtilisateurDTO() {
+	}
+	/**Constructeur d'utilisateurDTO par un utilisateur**/
+	public UtilisateurDTO(Utilisateur user) {
+		this.nomUser = user.getNomUser();
+		this.prenomUser = user.getPrenomUser();
+		this.mailUser = user.getMailUser();
+		this.roleUser = user.getRoleUser();
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(mailUser, nomUser, prenomUser, roleUser);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UtilisateurDTO other = (UtilisateurDTO) obj;
+		return Objects.equals(mailUser, other.mailUser) && Objects.equals(nomUser, other.nomUser)
+				&& Objects.equals(prenomUser, other.prenomUser) && Objects.equals(roleUser, other.roleUser);
+	}
+	@Override
+	public String toString() {
+		return "UtilisateurDTO [nomUser=" + nomUser + ", prenomUser=" + prenomUser + ", mailUser=" + mailUser
+				+ ", roleUser=" + roleUser + "]";
+	}
+	
+	
 }
--- JEE_REST/src/main/java/m2i/projet/JEE_REST/service/imp/UtilisateurServiceImp.java
+++ JEE_REST/src/main/java/m2i/projet/JEE_REST/service/imp/UtilisateurServiceImp.java
@@ -1,11 +1,89 @@
 package m2i.projet.JEE_REST.service.imp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import m2i.projet.JEE_REST.dto.UtilisateurDTO;
+import m2i.projet.JEE_REST.entity.Emprunt;
+import m2i.projet.JEE_REST.entity.Utilisateur;
 import m2i.projet.JEE_REST.repository.UtilisateurRepository;
 import m2i.projet.JEE_REST.service.UtilisateurService;
 
 public class UtilisateurServiceImp implements UtilisateurService {
 	@Autowired
 	UtilisateurRepository utilisateurRepository;
+
+	
+	@Override
+	public Utilisateur getUser(String email) {
+		Utilisateur user = new Utilisateur();
+		// recherche de l'utilisateur a supprimer
+		user = utilisateurRepository.getClientWithMail(email);
+		return user;
+	}
+	@Override
+	public UtilisateurDTO getUserDTO(String email) {
+		UtilisateurDTO userDTO = new UtilisateurDTO(utilisateurRepository.getClientWithMail(email));
+		return userDTO;
+	}
+
+	@Override
+	public void createUser(UtilisateurDTO userDTO) {
+		Utilisateur user = new Utilisateur();
+		user.setMailUser(userDTO.getMailUser());
+		user.setNomUser(userDTO.getNomUser());
+		user.setPrenomUser(userDTO.getPrenomUser());
+		user.setRoleUser(userDTO.getRoleUser());
+		utilisateurRepository.save(user);
+	}
+	
+	@Override
+	public void deleteUser(Utilisateur user) {
+		//supression de l'utilisateur via le repository
+		utilisateurRepository.delete(user);
+	}
+
+	@Override
+	public void deleteUserDTO(UtilisateurDTO userDTO) {
+		Utilisateur user = new Utilisateur();
+		// recherche de l'utilisateur a supprimer
+		user = utilisateurRepository.getClientWithMail(userDTO.getMailUser());
+		// supression de l'utilisateur
+		deleteUser(user);
+	}
+
+	@Override
+	public void updateUser(UtilisateurDTO updatedUSer) {
+		Utilisateur user = new Utilisateur();
+		//recupération de l'utilisateur depuuis l'utilisateur DTO
+		user = getUser(updatedUSer.getMailUser());
+		// mise a jour de l'utilisateur
+		utilisateurRepository.save(user);		
+	}
+
+	@Override
+	public List<Utilisateur> getUtilisateurs() {
+		return utilisateurRepository.findAll();
+	}
+	
+	@Override
+	public List<UtilisateurDTO> getUtilisateursDTO() {
+		List<Utilisateur>  users = new ArrayList<Utilisateur>();
+		List<UtilisateurDTO>  usersDTO = new ArrayList<UtilisateurDTO>();
+		for(Utilisateur u : users )
+		{
+			usersDTO.add(new UtilisateurDTO(u));
+		}
+		return usersDTO;
+	}
+
+	@Override
+	public List<Emprunt> getEmpruntFromUser(UtilisateurDTO user) {
+		return null;
+	}
+	
+
+
 }
