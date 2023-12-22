f8d57b7a5c5e9fd50b5718852569097798c9b877
Baptiste Hornecker <113470794+martinschongauer@users.noreply.github.com>
2023-12-22 15:41:33 +0100

    Update LivreRepository.java

--- JEE_REST/src/main/java/m2i/projet/JEE_REST/repository/LivreRepository.java
+++ JEE_REST/src/main/java/m2i/projet/JEE_REST/repository/LivreRepository.java
@@ -1,12 +1,19 @@
 package m2i.projet.JEE_REST.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import m2i.projet.JEE_REST.entity.Livre;
 
 @Repository
 public interface LivreRepository extends JpaRepository<Livre, String> {
-
-		public Livre  findByIsbnLivre(String ISBN); 
+    
+    @Query("SELECT l FROM Livre l WHERE titreLivre LIKE ?1 AND auteur LIKE ?2 AND editeur LIKE ?3")
+	List<Livre> getLivreFiltered(String s1, String s2, String s3);
+    
+	List<Livre> findByAuteurContaining(String auteur);
+	List<Livre> findByEditeurContaining(String editeur);
 }
