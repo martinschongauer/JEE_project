993a9212cf9b988a2896b01088aee9a9036c7e29
Baptiste Hornecker <113470794+martinschongauer@users.noreply.github.com>
2023-12-22 15:42:33 +0100

    Update LivreService.java

--- JEE_REST/src/main/java/m2i/projet/JEE_REST/service/LivreService.java
+++ JEE_REST/src/main/java/m2i/projet/JEE_REST/service/LivreService.java
@@ -1,5 +1,18 @@
 package m2i.projet.JEE_REST.service;
 
+import java.util.List;
+
+import m2i.projet.JEE_REST.dto.LivreDTO;
+import m2i.projet.JEE_REST.entity.Livre;
+
+
 public interface LivreService {
 
+	LivreDTO remplirDTO(Livre livre);
+	LivreDTO save(Livre livre);
+	LivreDTO findByISBN(String ISBN);
+	List<LivreDTO> findAll(String filt_titre, String filt_auteur, String filt_editeur);
+	void delete(String ISBN);
+	LivreDTO add(LivreDTO livre);
+	LivreDTO update(String ISBN, LivreDTO livreDTO);
 }
